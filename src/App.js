40ebff4dc2077c564a64cc48a3ebdc9329bb6d6c
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-23 19:59:56 +0100

    imported main page components and created routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Routes , Route } from 'react-router-dom'
+
+//page components
+import Home from './pages/Home'
+import About from './pages/About'
+import Rooms from './pages/Rooms'
+import Restaurant from './pages/Restaurant'
+import Blog from './pages/Blog'
+import Shop from './pages/Shop'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <div className="text-center font-bold">
-      Working...
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='about' element={<About/>}/>
+          <Route path='rooms' element={<Rooms/>}/>
+          <Route path='restaurant' element={<Restaurant/>}/>
+          <Route path='blog' element={<Blog/>}/>
+          <Route path='shop' element={<Shop/>}/>
+          <Route path='contact' element={<Contact/>}/>
+       </Routes>
     </div>
   );
 }
