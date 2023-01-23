9f8d9db979c6779fec2faec53b3fcd073485223a
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-23 20:03:21 +0100

    created basic header and footer and imported them

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ import Blog from './pages/Blog'
 import Shop from './pages/Shop'
 import Contact from './pages/Contact'
 
+//components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div className="text-center font-bold">
+      <Header/>
        <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='about' element={<About/>}/>
@@ -21,6 +26,7 @@ function App() {
           <Route path='shop' element={<Shop/>}/>
           <Route path='contact' element={<Contact/>}/>
        </Routes>
+       <Footer/>
     </div>
   );
 }
