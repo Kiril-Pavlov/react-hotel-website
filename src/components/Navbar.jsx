6f02529dd1abbbd217c63834badaf9e6d32b4d98
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-25 19:00:29 +0100

    extended the links list with sublinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,13 @@ import { links } from '../data/links'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex flex-row gap-2'>
       {links.map(link=>(
-        <Link to={link.linkTo} key={link.linkName}>
+        <Link 
+          to={link.linkTo} 
+          key={link.linkName}
+          className='border-2 py-5 px-5'
+        >
           {link.linkName}
         </Link>
       ))}
--- src/data/links.jsx
+++ src/data/links.jsx
@@ -1,30 +1,117 @@
 export const links = [
-    {
-      linkName: "Home",
-      linkTo: "/"
-    },
-    {
-      linkName: "About",
-      linkTo: "about"
-    },
-    {
-      linkName: "Rooms",
-      linkTo: "rooms"
-    },
-    {
-      linkName: "Restaurant",
-      linkTo: "restaurant"
-    },
-    {
-      linkName: "Blog",
-      linkTo: "blog"
-    },
-    {
-      linkName: "Shop",
-      linkTo: "shop"
-    },
-    {
-      linkName: "Contact",
-      linkTo: "contact"
-    }
-  ];
+  {
+    linkName: "Home",
+    linkTo: "/"
+  },
+  {
+    linkName: "About",
+    linkTo: "about",
+    submenu: true,
+    sublinks: [
+      {
+        sublinkName: "About Us",
+        sublinkTo: "about"
+      },
+      {
+        sublinkName: "FAQ",
+        sublinkTo: "faq"
+      },
+      {
+        sublinkName: "Galery",
+        sublinkTo: "galery"
+      },
+      {
+        sublinkName: "Services",
+        sublinkTo: "services"
+      },
+      {
+        sublinkName: "Page 404",
+        sublinkTo: "page404"
+      }
+    ]
+  },
+  {
+    linkName: "Rooms",
+    linkTo: "rooms",
+    submenu: true,
+    sublinks: [
+      {
+        sublinkName: "Room List",
+        sublinkTo: "room"
+      },
+      {
+        sublinkName: "FAQ",
+        sublinkTo: "faq"
+      },
+      {
+        sublinkName: "Check Availability",
+        sublinkTo: "availability"
+      },
+      {
+        sublinkName: "Create Account",
+        sublinkTo: "account"
+      }
+    ]
+  },
+  {
+    linkName: "Restaurant",
+    linkTo: "restaurant",
+    submenu: true,
+    sublinks: [
+      {
+        sublinkName: "Our Restaurant",
+        sublinkTo: "restaurant"
+      },
+      {
+        sublinkName: "Menu",
+        sublinkTo: "menu"
+      },
+      {
+        sublinkName: "Galery",
+        sublinkTo: "galery"
+      }
+    ]
+  },
+  {
+    linkName: "Blog",
+    linkTo: "blog",
+    submenu: true,
+    sublinks: [
+      {
+        sublinkName: "Blog List",
+        sublinkTo: "blog-list"
+      },
+      {
+        sublinkName: "Blog Post",
+        sublinkTo: "blog-post"
+      }
+    ]
+  },
+  {
+    linkName: "Shop",
+    linkTo: "shop",
+    submenu: true,
+    sublinks: [
+      {
+        sublinkName: "Shop",
+        sublinkTo: "shop"
+      },
+      {
+        sublinkName: "Cart",
+        sublinkTo: "cart"
+      },
+      {
+        sublinkName: "Checkout",
+        sublinkTo: "checkout"
+      },
+      {
+        sublinkName: "Product single",
+        sublinkTo: "product"
+      }
+    ]
+  },
+  {
+    linkName: "Contact",
+    linkTo: "contact"
+  }
+];
