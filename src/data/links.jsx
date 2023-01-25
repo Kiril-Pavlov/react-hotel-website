bf7f8e772a56fb3f747dbe835824cf60918f5470
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-25 18:50:49 +0100

    created link array in separate file

---
+++ src/data/links.jsx
@@ -0,0 +1,30 @@
+export const links = [
+    {
+      linkName: "Home",
+      linkTo: "/"
+    },
+    {
+      linkName: "About",
+      linkTo: "about"
+    },
+    {
+      linkName: "Rooms",
+      linkTo: "rooms"
+    },
+    {
+      linkName: "Restaurant",
+      linkTo: "restaurant"
+    },
+    {
+      linkName: "Blog",
+      linkTo: "blog"
+    },
+    {
+      linkName: "Shop",
+      linkTo: "shop"
+    },
+    {
+      linkName: "Contact",
+      linkTo: "contact"
+    }
+  ];
