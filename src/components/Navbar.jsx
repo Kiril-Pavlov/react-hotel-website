9cf0797736cae11b0663ee942b6f665926cd2dda
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-25 18:54:44 +0100

    mapped the links array in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { links } from '../data/links'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      {links.map(link=>(
+        <Link to={link.linkTo} key={link.linkName}>
+          {link.linkName}
+        </Link>
+      ))}
+    </div>
   )
 }
 
