900599cdfb525961ffc5fdcfa630538f7eaef8a8
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-27 20:28:23 +0100

    links styles and hover changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,15 +8,15 @@ const Navbar = () => {
     <nav className="hidden md:flex md:items-center md:justify-center gap-2">
       {links.map((link) => (
         <div className="py-1 group" key={link.linkName}>
-          <Link to={link.linkTo} className="py-5 px-5 md:cursor-pointer">
+          <Link to={link.linkTo} className="py-5 px-5 md:cursor-pointer text-white font-normal hover:text-primaryClr duration-500">
             {link.linkName}
           </Link>
           {link.submenu && (
             <div className="absolute top-15 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 hover:opacity-100 duration-300">
               <div className="overflow-hidden">
-                <div className="bg-black/90 w-5 h-5 relative left-5 rotate-45 top-3"></div>
+                <div className="bg-black/80 w-5 h-5 relative left-5 rotate-45 top-3"></div>
               </div>
-              <div className="flex flex-col bg-black/90 p-8 rounded-2xl gap-4">
+              <div className="flex flex-col bg-black/80 p-8 rounded-2xl gap-4">
                 {link.sublinks.map((sublink) => (
                   <Link
                     to={sublink.sublinkTo}
