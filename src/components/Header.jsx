fcee03c56bfbb96cb095f77a45dcd0897bac5333
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-29 13:44:32 +0100

    hamburger button toggle done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import CartModal from "./CartModal";
 import ColorButton from "./ColorButton";
 
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="bg-black/20 fixed h-24 w-full flex items-center justify-between text-amber-700 z-10">
       <Logo />
@@ -17,7 +19,11 @@ const Header = () => {
           <ColorButton text="book now" link="/" />
         </div>
         <div className="flex lg:hidden">
-          <GiHamburgerMenu  size={25} fill='white'/>
+          {isOpen ? (
+            <AiOutlineClose size={25} fill="white" onClick={()=>setIsOpen(!isOpen)}/>
+          ) : (
+            <GiHamburgerMenu size={25} fill="white" onClick={()=>setIsOpen(!isOpen)}/>
+          )}
         </div>
       </div>
     </header>
