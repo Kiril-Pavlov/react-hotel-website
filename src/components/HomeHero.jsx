a4ee8fd800c8a5e2ac77e640342010e814fe5faf
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-30 21:03:53 +0100

    change hero img for screen size

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import heroImg from '../assets/home-hero.jpg'
+import heroImgMobile from '../assets/home-hero-mobile.jpg'
 
 const HomeHero = () => {
   return (
     <div className="w-full h-screen relative overflow-hidden">
       {/* bg-cover bg-center bg-hero animate-background-zoom */}
-      <img src={heroImg} alt="" className='absolute animate-background-zoom w-full object-cover z-0'/>
+      <img src={heroImg} alt="" className='absolute hidden lg:block animate-background-zoom w-full object-cover z-0'/>
+      <img src={heroImgMobile} alt="" className='absolute lg:hidden animate-background-zoom w-full object-cover z-0'/>
     </div>
   )
 }
