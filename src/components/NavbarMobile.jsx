1d087d1ccc1d7f28196673cce400a3fad5e7b2d4
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-30 21:08:02 +0100

    imported links list and mapped main links

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import { links } from "../data/links";
 
 const NavbarMobile = ({hamStatus,setHamStatus}) => {
   return (
     <div className={`${hamStatus ? 'scale-x-100' : 'scale-x-0'} lg:hidden absolute w-10/12 left-[8.33%] top-28 bg-black/80 h-96 duration-300`}>
-
+      <div className='flex'>
+        {links.map(link=>(
+          <Link to={link.linkTo} key={link.linkName}>
+            {link.linkName}
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
