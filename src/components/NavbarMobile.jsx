1076fbaa385e0ce1486795125042a07923e8d0ac
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-31 21:28:41 +0100

    toggle visibility of sublinks done

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -16,17 +16,24 @@ const NavbarMobile = ({ hamStatus, setHamStatus }) => {
     >
       <div className="flex flex-col p-3 gap-2">
         {links.map((link) => (
-          <div className="flex items-center justify-between gap-2">
-            <Link
-              to={link.linkTo}
-              key={link.linkName}
-              className="flex items-center justify-left pl-5 text-white w-full h-10 border-b-[1px] uppercase"
-            >
-              {link.linkName}
-            </Link>
-            {link.submenu && (
-              <div className="border-2 w-10 h-10 flex items-center justify-center"
-              onClick={()=>(activeLink !== link.linkName ? setActiveLink(link.linkName):setActiveLink(""))}>
+          <div>
+            <div  className="flex flex-row items-center justify-between gap-2">
+              <Link
+                to={link.linkTo}
+                key={link.linkName}
+                className="flex items-center justify-left pl-5 text-white w-full h-10 border-b-[1px] uppercase"
+              >
+                {link.linkName}
+              </Link>
+              {link.submenu && (
+              <div
+                className="border-2 w-10 h-10 flex items-center justify-center"
+                onClick={() =>
+                  activeLink !== link.linkName
+                    ? setActiveLink(link.linkName)
+                    : setActiveLink("")
+                }
+              >
                 <BsChevronDown
                   size={25}
                   fill="white"
@@ -34,6 +41,18 @@ const NavbarMobile = ({ hamStatus, setHamStatus }) => {
                     activeLink === link.linkName ? "rotate-0" : "rotate-90"
                   } duration-300`}
                 />
+              </div>
+            )}
+            </div>
+            {link.submenu && (
+              <div>
+                {link.sublinks.map((sublink) => (
+                  <Link key={sublink.sublinkName} to={sublink.sublinkTo}
+                  className={`${activeLink === link.linkName ? "scale-x-100 block" : "scale-x-0 hidden"} flex items-center justify-center p-2 transition-all duration-300`}
+                  >
+                    {sublink.sublinkName}
+                  </Link>
+                ))}
               </div>
             )}
           </div>
