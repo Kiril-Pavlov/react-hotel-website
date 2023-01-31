3478e12ba105432706a4e3cee9d2cb6dfb26e4ab
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-31 21:04:01 +0100

    accordion toggle button done

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { links } from "../data/links";
 
-import {BsChevronLeft} from 'react-icons/bs'
+import { BsChevronDown } from "react-icons/bs";
 
 const NavbarMobile = ({ hamStatus, setHamStatus }) => {
   const [activeLink, setActiveLink] = useState("");
@@ -14,19 +14,28 @@ const NavbarMobile = ({ hamStatus, setHamStatus }) => {
         hamStatus ? "scale-x-100" : "scale-x-0"
       } lg:hidden absolute w-10/12 left-[8.33%] top-28 bg-black/80 h-96 duration-300`}
     >
-      <div className="flex flex-col">
+      <div className="flex flex-col p-3 gap-2">
         {links.map((link) => (
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between gap-2">
             <Link
               to={link.linkTo}
               key={link.linkName}
-              className="border-2 py-2 w-full h-10"
+              className="flex items-center justify-left pl-5 text-white w-full h-10 border-b-[1px] uppercase"
             >
               {link.linkName}
             </Link>
-            <div className="border-2 w-10 h-10 flex items-center justify-center">
-              <BsChevronLeft  size={25} fill="white"/>
-            </div>
+            {link.submenu && (
+              <div className="border-2 w-10 h-10 flex items-center justify-center"
+              onClick={()=>(activeLink !== link.linkName ? setActiveLink(link.linkName):setActiveLink(""))}>
+                <BsChevronDown
+                  size={25}
+                  fill="white"
+                  className={`${
+                    activeLink === link.linkName ? "rotate-0" : "rotate-90"
+                  } duration-300`}
+                />
+              </div>
+            )}
           </div>
         ))}
       </div>
