c1c06b6b0c0d7ef396a86de1f714501796e64f9c
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-12 17:00:11 +0100

    hero mobile positioning done

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -10,7 +10,7 @@ const HomeHero = () => {
       <img src={heroImg} alt="" className='absolute hidden lg:block animate-background-zoom h-full object-cover z-0'/>
       <img src={heroImgMobile} alt="" className='absolute lg:hidden animate-background-zoom h-full object-cover z-0'/>
       <div className='absolute z-10 w-full h-full bg-black/30'>
-          <div className='max-w-5xl mx-auto px-1/12 flex lg:flex-row lg:items-center lg:h-full gap-10'>
+          <div className='max-w-5xl mx-auto px-5 flex flex-col justify-center h-full lg:flex-row lg:items-center gap-10'>
             <div className='border-2 lg:w-7/12'>
               LEFT
             </div>
