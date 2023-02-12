dcb63e8a14146139e1d1bae71632bdbbdc245f03
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-12 16:56:24 +0100

    hero desktop positioning done

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -9,6 +9,16 @@ const HomeHero = () => {
       {/* bg-cover bg-center bg-hero animate-background-zoom */}
       <img src={heroImg} alt="" className='absolute hidden lg:block animate-background-zoom h-full object-cover z-0'/>
       <img src={heroImgMobile} alt="" className='absolute lg:hidden animate-background-zoom h-full object-cover z-0'/>
+      <div className='absolute z-10 w-full h-full bg-black/30'>
+          <div className='max-w-5xl mx-auto px-1/12 flex lg:flex-row lg:items-center lg:h-full gap-10'>
+            <div className='border-2 lg:w-7/12'>
+              LEFT
+            </div>
+            <div className='border-2 lg:w-5/12'>
+              RIGHT
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
