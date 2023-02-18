23d2cf02a1bda55fa7a62bad03a530ebabdbdfb0
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-18 21:07:49 +0100

    styled the free services sectionw/o the carousel

--- src/components/FreeServices.jsx
+++ src/components/FreeServices.jsx
@@ -3,14 +3,14 @@ import FreeServicesSlider from './FreeServicesSlider'
 
 const FreeServices = () => {
   return (
-    <div>
-        <div>
+    <div  className="bg-blackClr py-20 px-5">
+        <div className="text-4xl lg:text-5xl text-center w-full font-btnFont text-white pb-5">
             Kinsley is waiting for you
         </div>
-        <div>
+        <div className="text-center text-fontClrGrey font-semibold mb-10">
             Consectetur adipisicing elit. Nihil, illum voluptate eveniet ex fugit ea delectus, sed voluptatem.
         </div>
-        <div>
+        <div className='border-2'>
             <FreeServicesSlider/>
         </div>
     </div>
