36c46b3bc927dfd2fd625e1df5813cb9478b4a15
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-19 00:03:38 +0100

    handling number of slides acording to screen size don

--- src/components/FreeServicesSlider.jsx
+++ src/components/FreeServicesSlider.jsx
@@ -1,23 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
-import ServicesCard from './ServicesCard'
+import ServicesCard from "./ServicesCard";
 
-import {services} from "../data/services"
+import { services } from "../data/services";
 
 const FreeServicesSlider = () => {
-      // states for active slider and number of sliders on screen
+  // states for active slider and number of sliders on screen
   const [activeSlider, setActiveSlider] = useState(0);
   const [slidersNumber, setSlidersNumber] = useState(3);
-  return (
-    <div  className="flex items-center justify-center gap-5">
-       {services.map((card)=>(
-        <div>
-            <ServicesCard tag={card.tag} url={card.url}/>
-        </div>
-       )
-       )}
-    </div>
-  )
-}
 
-export default FreeServicesSlider
+      // useEffect hook to handle screen size at page load
+  useEffect(() => {
+    handleScreenSizes();
+  }, []);
+
+  // function to handle number of cards depending on screen size
+  const handleScreenSizes = () => {
+    if (window.innerWidth < 600) {
+      setSlidersNumber(1);
+    } else if (window.innerWidth < 1200) {
+      setSlidersNumber(2);
+    } else {
+      setSlidersNumber(3);
+    }
+  }
+
+    // slice arrays for card mapping
+    let modifiedSlides = services.slice(activeSlider,activeSlider+slidersNumber)
+
+  return (
+    <div className="flex items-center justify-between gap-5">
+      {modifiedSlides.map((card) => (
+        <div>
+          <ServicesCard tag={card.tag} url={card.url} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FreeServicesSlider;
--- src/components/servicesCard.jsx
+++ src/components/servicesCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ServicesCard = ({tag, url}) => {
   return (
-    <div className='flex items-center justify-center w-full h-80 bg-slate-400 my-16 overflow-hidden relative'>
-        <span className='font-bold text-md text-yellow-100 absolute top-2 right-2 bg-black p-2'>{tag}</span>
+    <div className='flex items-center justify-center w-full h-80 bg-blackClr my-16 overflow-hidden relative'>
+        <span className='font-bold text-md text-white absolute top-2 right-2 bg-black p-2'>{tag}</span>
         <img src={url} alt="content" className='w-full object-cover'/>
     </div>
   )
