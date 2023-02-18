4c32d33cb0283e56ee1461137e0953ed58feab03
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-18 21:50:29 +0100

    added states for active slider and sliders number

--- src/components/FreeServicesSlider.jsx
+++ src/components/FreeServicesSlider.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import ServicesCard from './ServicesCard'
 
 import {services} from "../data/services"
 
 const FreeServicesSlider = () => {
+      // states for active slider and number of sliders on screen
+  const [activeSlider, setActiveSlider] = useState(0);
+  const [slidersNumber, setSlidersNumber] = useState(3);
   return (
     <div  className="flex items-center justify-center gap-5">
        {services.map((card)=>(
