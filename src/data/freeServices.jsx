7da7cb871012360e7f627867121a503df567dd59
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-18 20:52:46 +0100

    added data for free services carousel

---
+++ src/data/freeServices.jsx
@@ -0,0 +1,33 @@
+import img1 from "../assets/free-services-1.jpg"
+import img2 from "../assets/free-services-2.jpg"
+import img3 from "../assets/free-services-3.jpg"
+import img4 from "../assets/free-services-4.jpg"
+import img5 from "../assets/free-services-5.jpg"
+import img6 from "../assets/free-services-6.jpg"
+
+export const footerGalery = [
+    {
+        url: img1,
+        tag: "Spa room"
+    },
+    {
+        url: img2,
+        tag: "Spa room"
+    },
+    {
+        url: img3,
+        tag: "Spa room"
+    },
+    {
+        url: img4,
+        tag: "Spa room"
+    },
+    {
+        url: img5,
+        tag: "Spa room"
+    },
+    {
+        url: img6,
+        tag: "Spa room"
+    }
+]
