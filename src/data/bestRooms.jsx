395dfdecbbf3f112672b8dcbe8d23b7d5100aa67
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-21 18:48:10 +0100

    fake data for the rooms created

--- src/data/bestRooms.jsx
+++ src/data/bestRooms.jsx
@@ -1,8 +1,15 @@
 import img1 from "../assets/b-room-1.jpg"
+import img2 from "../assets/b-room-2.jpg"
+import img3 from "../assets/b-room-3.jpg"
+import img4 from "../assets/b-room-4.jpg"
+import img5 from "../assets/b-room-5.jpg"
+import img6 from "../assets/b-room-6.jpg"
+import img7 from "../assets/b-room-7.jpg"
+import img8 from "../assets/b-room-8.jpg"
 
 export const bestRooms=[
     {
-        name: "Deluxe room",
+        name: "Deluxe Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
         price:"49",
         adults:"4",
@@ -11,48 +18,66 @@ export const bestRooms=[
         img: img1,
     },
     {
-        name: "Deluxe room",
+        name: "Standard Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
-        price:"49",
-        adults:"4",
-        size:"95",
+        price:"29",
+        adults:"2",
+        size:"50",
         linkTo: "/",
-        img: img1,
+        img: img2,
     },
     {
-        name: "Deluxe room",
+        name: "Double Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
-        price:"49",
-        adults:"4",
-        size:"95",
+        price:"39",
+        adults:"2",
+        size:"35",
         linkTo: "/",
-        img: img1,
+        img: img3,
     },
     {
-        name: "Deluxe room",
+        name: "Clasic Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
-        price:"49",
-        adults:"4",
-        size:"95",
+        price:"35",
+        adults:"3",
+        size:"42",
         linkTo: "/",
-        img: img1,
+        img: img4,
     },
     {
-        name: "Deluxe room",
+        name: "Triple Classcic Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
-        price:"49",
-        adults:"4",
-        size:"95",
+        price:"79",
+        adults:"3",
+        size:"42",
         linkTo: "/",
-        img: img1,
+        img: img5,
     },
     {
-        name: "Deluxe room",
+        name: "Bussiness Class Room",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
-        price:"49",
+        price:"59",
+        adults:"2",
+        size:"35",
+        linkTo: "/",
+        img: img6,
+    },
+    {
+        name: "Royal Class Room",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
+        price:"119",
+        adults:"2",
+        size:"50",
+        linkTo: "/",
+        img: img7,
+    },
+    {
+        name: "Superior Ocean Room",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptates.",
+        price:"19",
         adults:"4",
         size:"95",
         linkTo: "/",
-        img: img1,
+        img: img8,
     },
 ]
