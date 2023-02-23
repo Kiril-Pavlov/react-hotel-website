b72060110fafb3cb9b1840daa9d72cfd2cf503b3
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-23 21:40:21 +0100

    created the Around the hotel section and added heading paragraph and button

---
+++ src/components/AroundTheHotel.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import BorderButton from "./BorderButton"
+
+const AroundTheHotel = () => {
+  return (
+    <div className="bg-blackClr">
+      <div className=" pt-20 px-5 max-w-5xl mx-auto flex flex-col lg:flex-row items-start justify-between">
+        <div>
+          <div className="text-4xl lg:text-6xl text-left w-full font-btnFont text-white pb-5">
+            Around The Hotel
+          </div>
+          <div className="text-left text-fontClrGrey font-semibold mb-10 w-full lg:w-3/4">
+            Consectetur adipisicing elit. Nihil, illum voluptate eveniet ex
+            fugit ea delectus, sed voluptatem.
+          </div>
+        </div>
+        <BorderButton text="all services" link="/" />
+      </div>
+      <div className="px-5">
+        
+      </div>
+    </div>
+  )
+}
+
+export default AroundTheHotel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import HomeHero from '../components/HomeHero'
 import ShortAbout from '../components/ShortAbout'
 import Capacity from '../components/Capacity'
 import OurBestRooms from '../components/OurBestRooms'
+import AroundTheHotel from '../components/AroundTheHotel'
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <FreeServices/>
       <Capacity />
       <OurBestRooms />
+      <AroundTheHotel />
     </div>
   )
 }
