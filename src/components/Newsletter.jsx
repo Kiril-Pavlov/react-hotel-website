2fb571888e863edafe8576ea813bc8a8b0f4f2b3
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-27 14:24:31 +0100

    Newsletter component titleand button done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import BorderButton from './BorderButton'
+
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <div className="bg-lightBlackClr">
+      <div className=" pt-20 px-5 max-w-5xl mx-auto flex flex-col lg:flex-row items-start justify-between">
+        <div>
+          <div className="text-4xl lg:text-6xl text-left w-full font-btnFont text-white pb-5">
+            Our Newsletter
+          </div>
+          <div className="text-left text-fontClrGrey font-semibold mb-10 w-full lg:w-3/4">
+            Consectetur adipisicing elit. Nihil, illum voluptate eveniet ex
+            fugit ea delectus, sed voluptatem.
+          </div>
+        </div>
+        <BorderButton text="all publications" link="/" />
+      </div>
+      <div className="px-5">
+        
+      </div>
+    </div>
   )
 }
 
