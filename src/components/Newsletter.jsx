6c54b7b7145d6a8e674d138dfdf4e8a95327aa5a
Kiril-Pavlov <Kire_P@yahoo.com>
2023-03-10 20:40:08 +0100

    newsletter list created

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,7 +2,36 @@ import React from "react";
 
 import BorderButton from "./BorderButton";
 
+import img1 from "../assets/newsletter-1.jpg"
+import img2 from "../assets/newsletter-2.jpg"
+import img3 from "../assets/newsletter-3.jpg"
+import avatar1 from "../assets/avatar-1.jpg"
+
+
 const Newsletter = () => {
+  let posts = [
+    {
+      url : img1,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      title: "The Ultimate Guide to Traveling When You Have...",
+      paragraph: "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…"
+    },
+    {
+      url : img2,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      title: "The Best Travel Insurance Companies for Seniors",
+      paragraph: "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…"
+    },
+    {
+      url : img3,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      title: "18 Easy Steps for Planning Your Next Trip",
+      paragraph: "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…"
+    },
+  ]
   return (
     <div className="bg-lightBlackClr">
       <div className=" pt-20 px-5 max-w-5xl mx-auto flex flex-col lg:flex-row items-start justify-between">
