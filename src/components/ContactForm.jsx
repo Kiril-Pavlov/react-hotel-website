a28efea8108847b32dbba26d8bd3e2ccb370d1a7
Kiril-Pavlov <Kire_P@yahoo.com>
2023-03-15 19:07:20 +0100

    contact form content done

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+
+import ColorButton from './ColorButton'
 
 const ContactForm = () => {
   return (
-    <div>
-        form
+    <div className="bg-mediumBlackClr">
+      <h2>Write To Us!</h2>
+      <p>
+        Laborum accusantium libero commodi id officiis itaque esse adipisci,
+        necessitatibus asperiores, illo odio.
+      </p>
+      <form action="">
+        <div>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="E-mail" />
+        </div>
+        <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+        <div>
+          <ColorButton text="SEND" link="#"/>
+          <span>*We promise not to disclose your personal information to third parties.</span>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- src/components/ContactHero.jsx
+++ src/components/ContactHero.jsx
@@ -23,7 +23,7 @@ const ContactHero = () => {
         },
     ]
   return (
-    <div>
+    <div className='bg-mediumBlackClr'>
         <div className='flex h-[60vh] overflow-hidden relative bg-contact bg-cover bg-center'>
             <h2 className='flex items-center justify-center text-center text-3xl lg:text-8xl w-full text-white font-btnFont bg-gray-800/30 font-normal'>CONTACT US</h2>
         </div>
