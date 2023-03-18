7d214ad9ef6956376d361571a0323460cfb3d4be
Kiril-Pavlov <Kire_P@yahoo.com>
2023-03-18 21:10:43 +0100

    added newsletter component to Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+
 import BlogHero from '../components/BlogHero'
+import Newsletter from '../components/Newsletter'
 
 const Blog = () => {
   return (
     <div>
       <BlogHero />
+
+      <Newsletter />
     </div>
   )
 }
