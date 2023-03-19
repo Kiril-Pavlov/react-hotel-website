6da42da73fcf2e7079165ca3b9d2e1371fc9a0bb
Kiril-Pavlov <Kire_P@yahoo.com>
2023-03-19 16:11:55 +0100

    added test data for BlogPosts

---
+++ src/components/BlogPosts.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const BlogPosts = () => {
+  let posts = [
+    {
+      url: img1,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      date: "December 21, 2021",
+      title: "The Ultimate Guide to Traveling When You Have...",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+    {
+      url: img2,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      date: "December 21, 2021",
+      title: "The Best Travel Insurance Companies for Seniors",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+    {
+      url: img3,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+
+      date: "December 21, 2021",
+      title: "18 Easy Steps for Planning Your Next Trip",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+    {
+      url: img2,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      date: "December 21, 2021",
+      title: "The Best Travel Insurance Companies for Seniors",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+    {
+      url: img3,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+
+      date: "December 21, 2021",
+      title: "18 Easy Steps for Planning Your Next Trip",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+    {
+      url: img1,
+      userName: "Beshteya",
+      userAvatar: avatar1,
+      date: "December 21, 2021",
+      title: "The Ultimate Guide to Traveling When You Have...",
+      paragraph:
+        "Image for cattle earth. May one Which life divide sea. Commodi soluta minima nemo,…",
+    },
+  ];
+  return <div>BlogPosts</div>;
+};
+
+export default BlogPosts;
