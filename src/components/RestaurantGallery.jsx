40973080185b06297dbe753683c912fa654e83c1
Kiril-Pavlov <Kire_P@yahoo.com>
2023-03-22 21:44:42 +0100

    title and text done in RestaurantGallery section

--- src/components/RestaurantGallery.jsx
+++ src/components/RestaurantGallery.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const RestaurantGallery = () => {
   return (
-    <div>RestaurantGallery</div>
+    <div  className="bg-blackClr pt-20 px-5">
+        <div className="text-4xl lg:text-6xl text-center w-full font-btnFont text-white pb-5">
+            Restaurant Photo Gallery
+        </div>
+        <div className="text-center text-fontClrGrey font-semibold mb-10">
+            Consectetur adipisicing elit. Nihil, illum voluptate eveniet ex fugit ea delectus, sed voluptatem.
+        </div>
+        <div>
+            
+        </div>
+    </div>
   )
 }
 
